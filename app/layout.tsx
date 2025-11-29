3cbdc78f53ae2a6edbb8fe062562f28e0e69eb6d
Drew Baskin <drewbaskin@Drews-Mac-Studio.local>
2025-11-29 16:09:45 -0500

    Add Open Graph image and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,27 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Ambitious Admin',
+  title: 'Ambitious Social Admin Panel',
   description: 'Admin panel for Ambitious Social',
+  openGraph: {
+    title: 'Ambitious Social Admin Panel',
+    description: 'Admin panel for Ambitious Social',
+    type: 'website',
+    images: [
+      {
+        url: '/og-image.jpg',
+        width: 1500,
+        height: 500,
+        alt: 'Ambitious Social Admin Panel',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Ambitious Social Admin Panel',
+    description: 'Admin panel for Ambitious Social',
+    images: ['/og-image.jpg'],
+  },
 }
 
 export default function RootLayout({
